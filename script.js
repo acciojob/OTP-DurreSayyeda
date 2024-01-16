e0b39cf68183ca3924f736d758e37d77ab283fe3
Sayyeda Afshan <syed1995.sa@gmail.com>
2024-01-16 21:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function handleInput(input) {
+            if (input.value.length === 1) {
+                // Move focus to the next input
+                const nextInput = input.nextElementSibling;
+				
+                if (nextInput) {
+                    //nextInput.focus();
+					setTimeout(() => nextInput.focus(), 10);
+                }
+            } else if (input.value.length === 0) {
+                // Move focus to the previous input on backspace
+                const prevInput = input.previousElementSibling;
+                if (prevInput) {
+                   // prevInput.focus();
+					setTimeout(() => prevInput.focus(), 10);
+                }
+            }
+        }
